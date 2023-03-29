5249a6dfe6afa93fb7fa323c0be3711a8a56bbaa
wl <wll_133@163.com>
2023-03-29 22:52:49 +0800

    webpack

--- TS/webpacktest/src/index.ts
+++ TS/webpacktest/src/index.ts
@@ -1,1 +1,8 @@
 console.log('我是ts文件');
+import { a } from './view'
+console.log(a);
+const test1 = 'eeeee'
+console.log(test1);
+
+const aobj = {a:12}
+console.log(aobj);
--- TS/webpacktest/webpack.config.js
+++ TS/webpacktest/webpack.config.js
@@ -1,16 +1,47 @@
 const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: './src/index.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        clean: true
     },
     module: {
         // 指定loader规则
         rules: [{
             test: /\.ts$/,
-            use: 'ts-loader',
+            use: [{
+                loader:'babel-loader',
+                options:{
+                    presets:[
+                        [
+                            '@babel/preset-env',
+                            {
+                                targets:{
+                                    'chrome':'30'
+                                },
+                                'corejs':'3',
+                                'useBuiltIns':'usage'
+                            }
+                        ]
+                    ]
+                }
+            },'ts-loader'],
             exclude: /node_modules/
         }]
+    },
+    devServer: {
+        static: './dist',
+        open: true,
+      },
+    plugins:[
+        new HtmlWebPackPlugin({
+            template:'./public/index.html'
+        })
+    ],
+    // 定义哪些模块可以是引入的
+    resolve:{
+        extensions:['.ts','.js']
     }
 }
