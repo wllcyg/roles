5e372a20bfa398101ee8a50fe194d8e35472b3ec
GW00265321 <GW00265321@gmail.com>
2023-04-20 16:40:04 +0800

    init

--- TS/learn/index.js
+++ TS/learn/index.js
@@ -1,6 +1,6 @@
 /**
  * 格式化歌词
- * 
+ *
 */
 let wordsArr = []
 function paseLrc() {
@@ -13,39 +13,38 @@ function paseLrc() {
         }
         wordsArr.push(obj)
     })
-    console.log(wordsArr);
 }
 /**
  * 格式化时间
- * 
+ *
 */
 function paseTime(params) {
-    console.log(params);
     const [t1,t2] = params
     return +t1*60 + (+t2)
 }
 paseLrc()
 /**
  * 计算出当前播放是在第几秒
- * 
+ *
 */
 let doms = {
     audio:document.querySelector('audio'),
-    ul:document.querySelector('.container ul')
+    ul:document.querySelector('.container ul'),
+    liH:''
 }
 function findIndex() {
     let currentTime = doms.audio.currentTime
     for (let i = 0; i < wordsArr.length; i++) {
         if (currentTime < wordsArr[i].time) {
             return i - 1
-        }        
+        }
     }
     return wordsArr.length - 1
 }
 
 /**
  * 创建界面
- * */ 
+ * */
 function createElements() {
     let liFrogment = document.createDocumentFragment()
     for (let i = 0; i < wordsArr.length; i++) {
@@ -56,13 +55,43 @@ function createElements() {
     doms.ul.appendChild(liFrogment)
 }
 createElements()
-
+// 存放li
+let LI_LIST = []
 /**
  * 设置元素的偏移量
- * 
+ *
 */
-
 function setOffset() {
     let index = findIndex()
-    
+    if (LI_LIST[index]){
+      LI_LIST[index].classList.add('active')
+      doms.ul.style.transform = `translateY(${210 - (index * 36)}px)`
+    }
+
+    LI_LIST.forEach((e,ind) => {
+      if(ind !== index){
+        LI_LIST[ind].classList.remove('active')
+      }
+    })
 }
+setOffset()
+function play(){
+  LI_LIST = doms.ul.querySelectorAll('li')
+  doms.liH = LI_LIST[0].offsetHeight / 2
+  console.log('当前开始播放')
+}
+function  pause(){
+  console.log('暂停播放')
+}
+function playing(){
+  setOffset()
+}
+/**
+ * 添加事件监听进行处理
+ * */
+function eventListen(){
+  doms.audio.addEventListener('play',play)
+  doms.audio.addEventListener("pause", pause)
+  doms.audio.addEventListener('timeupdate',playing)
+}
+eventListen()
